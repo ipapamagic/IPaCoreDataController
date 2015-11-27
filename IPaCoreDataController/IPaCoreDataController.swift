ec98147d6310265044f27771321db21f6af80358
IPaPa <ipapamagic@gmail.com>
2015-11-27 17:08:54 +0800

    add products management feature

--- IPaCoreDataController/IPaCoreDataController.swift
+++ IPaCoreDataController/IPaCoreDataController.swift
@@ -14,7 +14,7 @@ public class IPaCoreDataController {
     var persistentStoreCoordinator:NSPersistentStoreCoordinator
     var managedObjectContext:NSManagedObjectContext
     init(dbName:String,modelName:String?,dbPath:String?) {
-        let dbFileName = dbName.stringByAppendingPathExtension(".sqlite")!
+        let dbFileName = (dbName as NSString).stringByAppendingPathExtension(".sqlite")!
         var momdUrl:NSURL?
         if let modelName = modelName {
             var modelPath:String? = nil
@@ -38,17 +38,17 @@ public class IPaCoreDataController {
             storePath = dbPath
         }
         else {
-            storePath = (NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true).first) as! String
+            storePath = (NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true).first)!
         }
-        storePath = storePath.stringByAppendingPathComponent(dbFileName)
+        storePath = (storePath as NSString).stringByAppendingPathComponent(dbFileName)
         let storeUrl = NSURL(fileURLWithPath: storePath)
-        var error:NSError?
         /*
         Set up the store.
         For the sake of illustration, provide a pre-populated default store.
         */
         persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
-        if (persistentStoreCoordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeUrl, options: nil, error: &error) == nil) {
+        do {
+            try persistentStoreCoordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeUrl, options: nil)
             /*
             Replace this implementation with code to handle the error appropriately.
             
@@ -60,61 +60,78 @@ public class IPaCoreDataController {
             Check the error message to determine what the actual problem was.
             */
             
-            print("Unresolved error \(error), \(error?.userInfo)")
+            
+        
+        }
+        catch let error as NSError {
+            print("Unresolved error \(error), \(error.userInfo)")
             abort();
             
         }
-        managedObjectContext = NSManagedObjectContext()
+        managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
         managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
     }
+    convenience init(dbName:String,modelName:String)
+    {
+        self.init(dbName:dbName,modelName:modelName,dbPath:nil)
+    }
+    convenience init(modelName:String)
+    {
+        self.init(dbName:modelName,modelName:modelName)
+    }
     public func deleteEntity(entityName:String) {
-        var fetchAllObjects = NSFetchRequest()
+        let fetchAllObjects = NSFetchRequest()
         fetchAllObjects.entity = NSEntityDescription.entityForName(entityName, inManagedObjectContext: managedObjectContext)
         fetchAllObjects.includesPropertyValues = false //only fetch the managedObjectID
-        var error:NSError?
-        var allObjects = managedObjectContext.executeFetchRequest(fetchAllObjects, error: &error)
-        
-        // uncomment next line if you're NOT using ARC
 
-        if let error = error {
-            print("\(error)");
-        }
-        if let allObjects = allObjects {
+        do {
+            let allObjects = try managedObjectContext.executeFetchRequest(fetchAllObjects)
             for object in allObjects {
                 if let object = object as? NSManagedObject {
                     managedObjectContext.deleteObject(object)
                 }
             }
+        } catch let error as NSError {
+
+            print("\(error)");
         }
+
+        
+
     }
     //MARK: core data stack
     public func save() {
-        var error:NSError?
-        if managedObjectContext.hasChanges && !managedObjectContext.save(&error)
-            {
-                /*
-                Replace this implementation with code to handle the error appropriately.
-                
-                abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
-                */
-                if let error = error {
-                    print("Unresolved error \(error), \(error.userInfo)");
-                }
-                abort();
+        do {
+            if managedObjectContext.hasChanges {
+                try managedObjectContext.save()
             }
+        } catch let error as NSError {
+            
+            print("Unresolved error \(error), \(error.userInfo)");
+           
+            abort();
+        }
     }
     
     
     public func insertNewObject(entityName:String) -> NSManagedObject {
-        return NSEntityDescription.insertNewObjectForEntityForName(entityName, inManagedObjectContext: managedObjectContext) as! NSManagedObject
+        return NSEntityDescription.insertNewObjectForEntityForName(entityName, inManagedObjectContext: managedObjectContext) 
     }
     public func deleteObject(object:NSManagedObject) {
         managedObjectContext.deleteObject(object)
         save()
     }
-    public func fetch(entityName:String,request:NSFetchRequest) -> [AnyObject]? {
-        request.entity = NSEntityDescription.entityForName(entityName, inManagedObjectContext: managedObjectContext)
-        return managedObjectContext.executeFetchRequest(request, error: nil)
+    public func fetch(request:NSFetchRequest) -> [AnyObject]? {
+        var fetchResult:[AnyObject]?
+        do {
+            try fetchResult = managedObjectContext.executeFetchRequest(request)
+        } catch let error as NSError {
+            
+            print("Unresolved error \(error), \(error.userInfo)");
+            
+            abort();
+        }
+        return fetchResult
     }
 }
 
